b603124ac63b358e117d919195f1a6a13fddca92
Endang Ismaya <endang.ismaya@gmail.com>
2023-10-07 16:56:20 +0700

    add initial EventDashboard

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,5 +1,12 @@
+import EventDashboard from '../../features/events/dashboard/EventDashboard';
+
 function App() {
-  return <h1>Welcome to Revents</h1>;
+  return (
+    <>
+      <h1>Welcome to Revents</h1>
+      <EventDashboard />
+    </>
+  );
 }
 
 export default App;
