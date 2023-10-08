22c0d024c4d5e0834d121af5352460100a3c02ca
Endang Ismaya <endang.ismaya@gmail.com>
2023-10-08 21:37:50 +0700

    add HomePage

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,20 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react';
 
 export default function NavBar() {
   return (
     <Menu inverted={true} fixed="top">
       <Container>
-        <MenuItem header>
+        <MenuItem header as={NavLink} to="/">
           <img src="/logo.png" alt="logo" />
           Re-vents
         </MenuItem>
-        <MenuItem name="Events" />
+        <MenuItem name="Events" as={NavLink} to="/events" />
         <MenuItem>
           <Button
             floated="right"
             positive={true}
             inverted={true}
             content="Create event"
+            as={NavLink}
+            to="/createEvent"
           />
         </MenuItem>
         <MenuItem position="right">
